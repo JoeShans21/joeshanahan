e4b8d345be1e35b17178ab49a36fa532f59d8d3b
Joe Shanahan <shans.joe@gmail.com>
2020-04-23 03:11:43 -0400

    dfdsfsfd

--- joe-drive/src/index.js
+++ joe-drive/src/index.js
@@ -1,10 +1,39 @@
 import React, { Component } from 'react'
 import ReactDOM from 'react-dom';
+import {Navbar, Nav, Card} from 'react-bootstrap'
 
 class App extends Component {
   render() {
+    fetch('http://joeshanahan.com/rpi2/allfiles').then((response) => {
+      return response.json();
+    }).then((data) => {
+      console.log(data)
+    })
     return (
-      <p>hello</p>
+      <div>
+        <Navbar bg="light" expand="lg">
+          <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="mr-auto">
+              <Nav.Link href="#home">Home</Nav.Link>
+              <Nav.Link href="#link">Link</Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Navbar>
+        <Card style={{ width: '18rem' }}>
+          <Card.Body>
+            <Card.Title>Card Title</Card.Title>
+            <Card.Subtitle className="mb-2 text-muted">Card Subtitle</Card.Subtitle>
+            <Card.Text>
+              Some quick example text to build on the card title and make up the bulk of
+              the card's content.
+            </Card.Text>
+            <Card.Link href="#">Card Link</Card.Link>
+            <Card.Link href="#">Another Link</Card.Link>
+          </Card.Body>
+        </Card>
+      </div>
     );
   }
 }
